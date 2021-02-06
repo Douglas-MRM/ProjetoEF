e58796bf1172b54280358c5b63852417c5edb9ba
Douglas-MRM <douglaswwaff@gmail.com>
2021-02-06 19:08:45 -0400

    Envio

---
+++ ProjetoEF/src/conexao/ConexaoMySql.java
@@ -0,0 +1,27 @@
+package conexao;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class ConexaoMySql {
+
+    private static final String url = "jdbc:mysql://localhost/bd_sisBike";
+    private static final String user = "root";
+    private static final String password = "root";                             //
+    //private static final String password = "S01l05s23l07";
+
+    public static Connection getConexao() {
+        Connection con = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            con = DriverManager.getConnection(url, user, password);
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao conectar!" + e.getMessage());
+        }
+
+        return con;
+    }
+}
